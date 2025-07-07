4b940fe8dcac083bbd6ed2eef929dbae5c4384ec
aovercash <amberovercash1@gmail.com>
2025-07-07 10:56:19 -0400

    Added gradebook.html and gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,8 @@
+Function fetchGradeData() {
+	console.log("Fetching grade data...");
+}
+Function populateGradebook(data) {
+	console.log("Populating gradebook with data:", data);
+}
+const GradeData = fetchGradeData();
+populateGradebook(GradeData);
